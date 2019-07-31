8019480c2a257473a8b886119f3461d595bac6e0
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-31 19:25:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 #string is "Hello there how are you"
 def reverse_each_word(sentence)
 string=sentence.split('')
-
+string.to_a.reverse
 #reverse_string=''
 #string.each |letter| do
   #reverse_string<<string.unshift
