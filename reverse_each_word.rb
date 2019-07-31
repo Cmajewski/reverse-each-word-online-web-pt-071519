a3ca526def4458d9016008b4bcca009028eb8373
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-07-31 19:56:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(sentence)
 string=sentence.split
 reversed_sentence=""
-string.reverse_each_word {|letter|reversed_sentence<<letter}
+string.reverse_each {|letter|reversed_sentence<<letter}
   #letter.shift
 #reverse_string=''
 #string.collect |letter| do
